42ae68a6b298b3db4b90f134a362915daf4f979a
DESIKA <113323277+Desika1706@users.noreply.github.com>
2022-09-15 13:50:21 +0530

    Add files via upload

---
+++ 110.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+class Overload
+ {
+ public:
+ 		int operator ++(int count)
+ 		{
+ 			count = count + 1 ;
+ 			return count;
+ 		};
+ 		int operator --(int count)
+ 		{
+ 			count = count + 1 ;
+ 			return count;
+ 		};
+ };
+                     
+ int main()
+ {
+ 	int a1 = 0 ;
+ 	char choice;
+ 	while ( 1 )
+ 	{
+ 		cout << "Enter your choice : 1. PRODUCER ( E )  2. CONSUMER( L)  3. EXIT(X) 4. Print (P) ."<<endl;
+	
+ 		cin >> choice;
+
+ 		switch ( choice )
+ 		{
+ 			case 'E' :
+ 				a1 = ++(a1);
+ 				cout << "Total number of PRODUCER is: ";
+ 				cout << a1;
+ 				cout << endl;
+ 				break;
+ 			case 'L' : 
+ 			    if(a1==0)
+				 cout<<"there is no producer so no consumer \n";
+				else
+				a1= ++(a1);
+ 				cout << "\n Total number of CONSUMER is: ";
+ 				cout << a1;
+ 				cout << endl;
+ 				break;
+ 			case 'P' :
+ 				cout << "Total number of person in PERSONS is: ";
+ 				cout << a1;
+ 				cout << endl;
+ 				break;
+
+ 			case 'X' :
+ 				cout << "THEY ENTER EXIT"<< endl;
+ 				exit(0);
+ 			default :
+ 				cout << "Enter a correct choice."<< endl;
+ 				break;
+ 		}
+ 	}
+ 	return 0;
+ }
